6c8c4417289da4d9fde57b20a6b1a13057af6b72
KatarzynaZoladek <katarzynawojcik47@gmail.com>
2024-02-13 10:45:23 +0100

    code correction

--- JS/script.js
+++ JS/script.js
@@ -65,13 +65,13 @@
   const renderTasks = () => {
     const taskToHTML = task => `
       <li class="tasks__item${task.done && hideDoneTasks ? " tasks__item--hidden" : ""} js-task">
-      <button class="tasks__button tasks__button--toggleDone js-toggleDone">
+        <button class="tasks__button tasks__button--toggleDone js-toggleDone">
         ${task.done ? "✓" : ""}
-      </button>
-      <span class="tasks__content${task.done ? " tasks__content--done" : ""}">
+        </button>
+        <span class="tasks__content${task.done ? " tasks__content--done" : ""}">
         ${task.content}
-      </span>
-      <button class="tasks__button tasks__button--remove js-remove"> 🗑︎ </button>
+        </span>
+        <button class="tasks__button tasks__button--remove js-remove"> 🗑︎ </button>
       </li>
       `;
 
